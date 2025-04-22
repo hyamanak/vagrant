197b6825c107548575fbc49ab2dbd64e5b9e321c
Hiroshi Yamanaka <63842038+hyamanak@users.noreply.github.com>
2025-04-22 22:08:29 +0900

    Update setup-ansible.sh

--- provisioning/setup-ansible.sh
+++ provisioning/setup-ansible.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -eux
 
+# 必要パッケージのインストール
 dnf install -y ansible openssh-clients
+
+# vagrantユーザーの鍵生成（もし未生成なら）
+if [ ! -f /home/vagrant/.ssh/id_rsa ]; then
+  sudo -u vagrant ssh-keygen -t rsa -b 2048 -N "" -f /home/vagrant/.ssh/id_rsa
+fi
+
+# 全マネージドノードに鍵を配布（初期パスワード: vagrant）
+for host in 192.168.57.10 192.168.57.11 192.168.57.12; do
+  sshpass -p vagrant ssh-copy-id -o StrictHostKeyChecking=no vagrant@$host
+done
